8c70055f8bfbc67db92f86b55ee70779b41ff5c6
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2026-01-30 21:13:14 -0800

    Chapter 2: Loops in Python

---
+++ 01-python-refresher/11-loops/code.py
@@ -0,0 +1,16 @@
+# secret = 42
+# user_input = -1
+
+# while user_input != secret:
+#   user_input = int(input("Guess a number: "))
+
+#   if user_input == secret:
+#     print("WINNER")
+#   else:
+#     print("Try again")
+  
+
+numbers = {1, 2, 3, 4, 5}
+
+for num in numbers:
+  print(num)
