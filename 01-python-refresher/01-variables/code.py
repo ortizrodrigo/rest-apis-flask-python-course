272ee5cc073c8395e57bb2abf82bac2b03b42362
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2026-01-30 18:27:04 -0800

    Chapter 2: Variables in Python

---
+++ 01-python-refresher/01-variables/code.py
@@ -0,0 +1,19 @@
+x = 15
+price = 9.99
+discount = 0.2
+result = price * (1-discount)
+print(result)
+
+name = "Rodrigo"
+print(name)
+print(name * 2)
+print(name + name)
+
+a = 25
+b = a
+
+print(a, b)
+
+b = 17
+
+print(a, b)
