732f3c42275ff69925653ae3f74ec289b4ffb45b
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2026-01-30 20:46:21 -0800

    Chapter 2: String formatting in Python

---
+++ 01-python-refresher/02-string-formatting/code.py
@@ -0,0 +1,8 @@
+name = "Bob"
+greeting = "Hello, {}"
+with_name = greeting.format(name)
+print(with_name)
+
+longer_phrase = "Hello, {}. Today is {}."
+formatted = longer_phrase.format("Rolf", "Monday")
+print(formatted)
