6aa22d6b57f0c69eee6d0b6bc49be8e366d8f894
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2026-01-30 21:07:09 -0800

    Chapter 2: If statements

---
+++ 01-python-refresher/08-if-statements/code.py
@@ -0,0 +1,8 @@
+day_of_the_week = input("What day of the week is it today?")
+
+if day_of_the_week == "Monday":
+  print("Have a great start to your week!")
+elif day_of_the_week == "Tuesday":
+  print("elif")
+else:
+  print("else")
