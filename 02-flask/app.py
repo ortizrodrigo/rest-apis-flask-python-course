f689ad51741afa67626f802389e6e1c6dfbd7bb3
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2026-02-01 10:05:55 -0800

    Chapter 3: Your first REST API endpoint

--- 02-flask/app.py
+++ 02-flask/app.py
@@ -1,3 +1,19 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+stores = [
+  {
+    "name": "My Store",
+    "items": [
+      {
+        "name": "Chair",
+        "price": 15.99
+      }
+    ]
+  }
+]
+
+@app.route("/store", methods=["GET"]) # http://127.0.0.1:5000/store (GET)
+def get_stores():
+  return {"stores": stores}
