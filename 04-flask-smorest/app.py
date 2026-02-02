2ed28fb4d9289554a9863b3ec0ecc6e6e9b07107
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2026-02-01 18:00:24 -0800

    Chapter 5: New endpoints for our first REST API

--- 04-flask-smorest/app.py
+++ 04-flask-smorest/app.py
@@ -5,9 +5,17 @@ from db import items, stores
 
 app = Flask(__name__)
 
+# Stores
 @app.get("/store")
 def get_stores():
   return {"stores": list(stores.values())}
+
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+  try:
+    return stores[store_id]
+  except KeyError:
+    abort(404, message="Store not found.")
 
 @app.post("/store")
 def create_store():
@@ -25,9 +33,25 @@ def create_store():
   stores[store_id] = store
   return store, 201
 
+@app.delete("/store/<string:store_id>")
+def delete_store(store_id):
+  try:
+    del stores[store_id]
+    return {"message": "Store deleted."}
+  except KeyError:
+    abort(404, message="Store not found.")
+
+# Items
 @app.get("/item")
 def get_all_items():
   return {"items": list(items.values())}
+
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+  try:
+    return items[item_id]
+  except KeyError:
+    abort(404, message="Item not found.")
 
 @app.post("/item")
 def create_item():
@@ -51,16 +75,26 @@ def create_item():
 
   return item, 201
 
-@app.get("/store/<string:store_id>")
-def get_store(store_id):
-  try:
-    return stores[store_id]
-  except KeyError:
-    abort(404, message="Store not found.")
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+  item_data = request.get_json()
 
-@app.get("/item/<string:item_id>")
-def get_item(item_id):
+  required_keys = ["price", "name"]
+  missing_keys = [k for k in required_keys if k not in item_data]
+  if missing_keys:
+    abort(400, message=f"Missing required keys: {', '.join(missing_keys)}")
+
   try:
-    return items[item_id]
+    item = items[item_id]
+    item |= item_data
+    return item
+  except KeyError:
+    abort(404, message="Item not found.")
+
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+  try:
+    del items[item_id]
+    return {"message": "Item deleted."}
   except KeyError:
     abort(404, message="Item not found.")
