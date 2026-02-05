852b7a5920d00bd1bb71ba4bd994f066735c1b38
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2026-02-04 22:59:26 -0800

    Chapter 6: How to find models in the database by ID or return a 404

--- 05-flask-sql-alchemy/resources/store.py
+++ 05-flask-sql-alchemy/resources/store.py
@@ -14,17 +14,12 @@ blp = Blueprint("stores", __name__, description="Operations on stores")
 class Store(MethodView):
   @blp.response(200, StoreSchema)
   def get(self, store_id):
-    try:
-      return stores[store_id]
-    except KeyError:
-      abort(404, message="Store not found.")
+    store = StoreModel.query.get_or_404(store_id)
+    return store
 
   def delete(self, store_id):
-    try:
-      del stores[store_id]
-      return {"message": "Store deleted."}
-    except KeyError:
-      abort(404, message="Store not found.")
+    store = StoreModel.query.get_or_404(store_id)
+    raise NotImplementedError("Deleting a store is not implemented.")
 
 @blp.route("/store")
 class StoreList(MethodView):
@@ -36,7 +31,7 @@ class StoreList(MethodView):
   @blp.response(200, StoreSchema)
   def post(self, store_data):
     store = StoreModel(**store_data)
-    
+
     try:
       db.session.add(store)
       db.session.commit()
