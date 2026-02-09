4f8dfcbf8f898b65277e177dcfb665e88bd0a74d
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2026-02-08 21:56:05 -0800

    Chapter 8: How to add logout to the REST API

--- 07-flask-jwt-extended/resources/user.py
+++ 07-flask-jwt-extended/resources/user.py
@@ -1,10 +1,11 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt
 from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 
 from db import db
+from blocklist import BLOCKLIST
 from models import UserModel
 from schemas import UserSchema
 
@@ -44,6 +45,14 @@ class UserLogin(MethodView):
       pbkdf2_sha256.verify(user_data["password"], dummy_hash) # mitigate timing attack
     
     abort(401, message="Invalid credentials.")
+
+@blp.route("/logout")
+class UserLogout(MethodView):
+  @jwt_required()
+  def post(self):
+    jti = get_jwt()["jti"]
+    BLOCKLIST.add(jti)
+    return {"message": "Successfully logged out."}
 
 @blp.route("/user/<int:user_id>")
 class User(MethodView):
